fb6ac7a5b4638706f62be0fdcc47282d9a1959cd
Vu Viet <vudinhviett@gmail.com>
2024-06-22 23:41:01 +0700

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include <cstring>
 #include <cstdlib>
 
 
 int print_menu_get_selection(int choice){
     while (1) {
-        printf("Student Management Program\n");
         printf("================================\n");
         printf("1. Enter Student Data\n2. Print All Student Data\n3. Find Max Grade\n4. Find Min Grade\n5. Exit\n");
         printf("================================\n");
@@ -20,36 +18,35 @@ int print_menu_get_selection(int choice){
     return choice;
 }
 
-int enter_student_data(long student_ids[], float student_grades[], int students_size) {
+int enter_student_data(int student_ids[], float student_grades[], int students_size) {
     float student_grade;
     int i;
-    system("cls");
+    
     for (i = 0; i < students_size; i++) {
         long student_id;
         printf("Enter ID Student %d: ", i+1);
-        scanf("%ld", &student_id);
+        scanf("%d", &student_id);
         printf("Enter Grade Student %d: ", i+1);
         scanf("%f", &student_grade);
-        student_ids[i] = student_id; // Assign student ID to the array
-        student_grades[i] = student_grade; // Assign student grade to the array
-        printf("\n---> Student %d: ID %ld - Grade %.2f", i+1, student_ids[i], student_grades[i]);
+        student_ids[i] = student_id;
+        student_grades[i] = student_grade;
+        printf("\n---> Student %d: ID %d - Grade %.2f", i+1, student_ids[i], student_grades[i]);
         printf("\n================================\n");
-
     }
     printf("\n================================\n");
     return 0;
 }
-int print_student_data(long student_ids[], float student_grades[], int students_size) {
-    system("cls");
+int print_student_data(int student_ids[], float student_grades[], int students_size) {
+    
     printf("All Student Data:\n");
-    printf("No\t\tID\t\t\t\tGrade\n");
+    printf("No\t\tID\t\t\tGrade\n");
     for (int i = 0; i < students_size; i++) {
-        printf("%d\t\t%ld\t\t\t\t%.2f\n", i+1, student_ids[i], student_grades[i]);
+        printf("%d\t\t%d\t\t\t%.2f\n", i+1, student_ids[i], student_grades[i]);
     }
     printf("\n================================\n");
     return 0;
 }
-int find_max_grade(long student_ids[], float student_grades[], int students_size) {
+int find_max_grade(int student_ids[], float student_grades[], int students_size) {
     float max_grade = student_grades[0];
     long student_id_highest;
     for (int i = 0; i < students_size; i++) {
@@ -58,11 +55,11 @@ int find_max_grade(long student_ids[], float student_grades[], int students_size
             student_id_highest = student_ids[i];
         }
     }
-    printf("\nHighest grade: ID %ld - Grade %.2f", student_id_highest, max_grade);
+    printf("\nHighest grade: ID %d - Grade %.2f", student_id_highest, max_grade);
     printf("\n================================\n");
     return 0;
 }
-int find_min_grade(long student_ids[], float student_grades[], int students_size) {
+int find_min_grade(int student_ids[], float student_grades[], int students_size) {
     float min_grade = student_grades[0];
     long student_id_lowest;
     for (int i = 0; i < students_size; i++) {
@@ -71,21 +68,19 @@ int find_min_grade(long student_ids[], float student_grades[], int students_size
             student_id_lowest = student_ids[i];
         }
     }
-    printf("\nLowest grade: ID %ld - Grade %.2f", student_id_lowest, min_grade);
+    printf("\nLowest grade: ID %d - Grade %.2f", student_id_lowest, min_grade);
     printf("\n================================\n");
     return 0;
 }
 
 int main() {
-    system("cls");
     printf("Student Management Program\n");
     printf("================================\n");
     printf("Enter the number of students: ");
     int student_count;
     scanf("%d", &student_count);
-    long student_ids[student_count];
+    int student_ids[student_count];
     float student_grades[student_count];
-    system("cls");
     while (1) {
 
         int choice;
